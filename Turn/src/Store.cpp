f115ec34bd8f4240c5db24872bb1b3f4f5e7f2ff
SenatorConfer <43549605+SenatorConfer@users.noreply.github.com>
2020-02-27 18:57:41 -0500

    Implemented purchase of multiple items in a single order

--- Turn/src/Store.cpp
+++ Turn/src/Store.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 using namespace Common;
 
-const unsigned short arrowsCost = 200;
+const unsigned short arrowsCost = 40;
 const unsigned short bombCost = 100;
 const unsigned short potionCost = 150;
 const unsigned short whetstoneCost = 20;
@@ -17,11 +17,13 @@ const unsigned short whetstoneCost = 20;
 //Messages
 const string lowCoinsMessage = " Insufficient Coins !";
 const string invalidChoice = " Invalid Choice !";
+const string invalidAmount = " Invalid Amount ! Choose Between 1 - 9 ! ";
 
 void Store::StoreFront(Player* _Player) {
 
 	unsigned short choice = 0;
 	unsigned short coins;
+	unsigned short amount = 0;
 
 	do
 	{
@@ -32,7 +34,7 @@ void Store::StoreFront(Player* _Player) {
 		cout << endl << " Jeremy's Tools Store" << endl;
 		cout << " \"" << random_greeting() << "\"" << endl << endl;
 		cout << " ----ITEM----------------COST------ " << endl << endl;
-		cout << " 1) Arrows x 5\t\t " << arrowsCost << endl;
+		cout << " 1) Arrow\t\t " << arrowsCost << endl;
 		cout << " 2) Bomb\t\t " << bombCost << endl;
 		cout << " 3) Potion\t\t " << potionCost << endl;
 		cout << " 4) Whetstone\t\t " << whetstoneCost << endl << endl;
@@ -40,58 +42,99 @@ void Store::StoreFront(Player* _Player) {
 		cout << " ---------------------------------- " << endl << endl;
 		cout << " What do you want to buy today?" << endl << endl << " ";
 		choice = input();
-		cout << endl;
+		cout << endl << " ";
+		if (choice > 0 && choice < 5) {
+			cout << choice << endl;
+			cout << " How many do you want to buy today?" << endl << endl << " ";
+			amount = input();
+		}
 		switch (choice)
 		{
 		case ITEMTYPE::ARROWS:
-			if (coins < arrowsCost)
+			if (amount == 0) {
+				cout << invalidAmount << endl;
+			}
+			else if (coins < (arrowsCost*amount))
 			{
 				cout << lowCoinsMessage << endl;
 			}
 			else
 			{
-				cout << " 5 Arrows purchased ! " << endl;
-				_Player->AddStoreItemToInventory(ITEMTYPE::ARROWS);
-				_Player->LoseCoins(arrowsCost);
+				cout << amount << " Arrow";
+				if (amount > 1) {
+					cout << "s";
+				}
+				cout << " purchased !" << endl;
+				for (int i = 0; i < amount; i++) {
+					_Player->AddStoreItemToInventory(ITEMTYPE::ARROWS);
+					_Player->LoseCoins(arrowsCost);
+				}
 			}
 			Sleep(SLEEP_MS);
 			break;
 		case ITEMTYPE::BOMB:
-			if (coins < bombCost)
+			if (amount == 0) {
+				cout << invalidAmount << endl;
+			}
+			else if (coins < (bombCost*amount))
 			{
 				cout << lowCoinsMessage << endl;
 			}
 			else
 			{
-				cout << " Bomb purchased !" << endl;
-				_Player->AddStoreItemToInventory(ITEMTYPE::BOMB);
-				_Player->LoseCoins(bombCost);
+				cout << amount << " Bomb";
+				if (amount > 1) {
+					cout << "s";
+				}
+				cout << " purchased !" << endl;
+				for (int i = 0; i < amount; i++) {
+					_Player->AddStoreItemToInventory(ITEMTYPE::BOMB);
+					_Player->LoseCoins(bombCost);
+				}
 			}
 			Sleep(SLEEP_MS);
 			break;
 		case ITEMTYPE::POTION:
-			if (coins < potionCost)
+			if (amount == 0) {
+				cout << invalidAmount << endl;
+			}
+			else if (coins < (potionCost*amount))
 			{
 				cout << lowCoinsMessage << endl;
 			}
 			else
 			{
-				cout << " Potion purchased !" << endl;
-				_Player->AddStoreItemToInventory(ITEMTYPE::POTION);
-				_Player->LoseCoins(potionCost);
+				cout << amount << " Potion";
+				if (amount > 1) {
+					cout << "s";
+				}
+				cout << " purchased !" << endl;
+				for (int i = 0; i < amount; i++) {
+					_Player->AddStoreItemToInventory(ITEMTYPE::POTION);
+					_Player->LoseCoins(potionCost);
+				}
 			}
 			Sleep(SLEEP_MS);
 			break;
 		case ITEMTYPE::WHETSTONE:
-			if (coins < whetstoneCost)
+			if (amount == 0) {
+				cout << invalidAmount << endl;
+			}
+			else if (coins < (whetstoneCost*amount))
 			{
-				cout << lowCoinsMessage << endl;
+				cout << amount << lowCoinsMessage << endl;
 			}
 			else
 			{
-				cout << " Whetstone purchased !" << endl;
-				_Player->AddStoreItemToInventory(ITEMTYPE::WHETSTONE);
-				_Player->LoseCoins(whetstoneCost);
+				cout << amount << " Whetstone";
+				if (amount > 1) {
+					cout << "s";
+				}
+				cout << " purchased !" << endl;
+				for (int i = 0; i < amount; i++) {
+					_Player->AddStoreItemToInventory(ITEMTYPE::WHETSTONE);
+					_Player->LoseCoins(whetstoneCost);
+				}
 			}
 			Sleep(SLEEP_MS);
 			break;
