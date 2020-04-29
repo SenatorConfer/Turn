2e6e75c74f55dcc06e3366de8e0ead9f83a554bb
Hayden <60161699+HRRichter96@users.noreply.github.com>
2020-04-28 21:14:28 -0400

    Update metapod.cpp for documentation

--- Turn/src/EnemyTypes/Metapod.cpp
+++ Turn/src/EnemyTypes/Metapod.cpp
@@ -1,5 +1,10 @@
 #include "../../include/EnemyTypes/Metapod.h"
-
+/*!
+    This Enemy Class satisfies User Story E.
+    Req 5.0 - Implementation of the metapod enemy has been met as you can encounter it during the game.
+    Req 5.0.1 - The Metapod has a high defense in that it heals a lot more than normal and also meets that it has no attack.
+    Link - github.com/SenatorConfer/Turn/blob/master/Requirements.md
+*/
 Metapod::Metapod() {
     name = "Metapod";
     ExperienceAmount = 20;
@@ -18,6 +23,9 @@ int Metapod::ReturnRiskAttackDamage() {
     return 0;
 }
 
+/*!
+    Metapod has only a strong heal.
+*/
 int Metapod::ReturnHealAmount() {
     return 1 + rand() % 10;
 }
